5274ed698143648fc2af6d0d432c6a156104381d
Joorch <jacortez_94@hotmail.com>
2021-01-28 22:59:49 -0600

    agregando script de tareas(prueba 1) y realizando re acomodo de  archivos

---
+++ GGJ2021/Assets/Scripts/Tasks/TaskManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="New Task", menuName = "Task")]
+public class TaskManager : MonoBehaviour
+{ 
+    public Task task; 
+    public Player player; 
+}
