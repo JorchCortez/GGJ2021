938e3f8d07b0e3a6ba7c3f1beb4a0e5dbdacbe90
Joorch <jacortez_94@hotmail.com>
2021-01-30 00:21:20 -0600

    agregando el script de interaccion con objetos y la modificacion de color

---
+++ GGJ2021/Assets/Scripts/Interactibles/InteractionHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHandler : MonoBehaviour
+{
+
+    public bool IsEnabled = false;
+
+    [SerializeField]
+    SpriteRenderer coloredImage;
+    [SerializeField]
+    GameObject blueImage;
+
+    public int duration = 150;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Colorize();
+        }
+    }
+
+    public void Colorize()
+    {
+        StartCoroutine(ColorizeObject(duration));
+    }
+
+    IEnumerator ColorizeObject( float duration)
+    {
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / duration; 
+            coloredImage.color += new Color(255, 255, 255, Mathf.Lerp(0, 1, normalizedTime));
+            yield return null;
+        } 
+    }
+}
